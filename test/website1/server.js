6e41c4e57c770fc1c73d6df4c61fb3e741925445
TeejayBala <balakumar.sivakumar@zohocorp.com>
2025-08-07 11:39:07 +0530

    website update

--- test/website1/server.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-const fs = require('fs');
-
-https.createServer({
-    cert: fs.readFileSync('./localhost.crt'),
-    key: fs.readFileSync('./localhost.key')
-}, (req, res) => {
-}).listen(4430);
-console.log("Server listening on https://localhost:4430/");

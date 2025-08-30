4477a81473bc27f13e68252c76e069c9d6c67310
TeejayBala <balakumar.sivakumar@zohocorp.com>
2025-08-30 22:40:55 +0530

    website update

--- test/shopping_live/common.js
+++ test/shopping_live/common.js
@@ -5,18 +5,20 @@ window.common = {
     logout : () => {
 
     },
-    add_to_cart : (id, name, price) => {
-        dataLayer.push({
-            event: 'add_to_cart',
-            ecommerce: {
-                items: [{
-                    item_id: "cart_"+id,
-                    item_name: name,
-                    price: price,
-                    quantity: 1
-                }]
-            }
-        });
+    add_to_cart : (product) => {
+        if (product) {
+            dataLayer.push({
+                event: 'add_to_cart',
+                ecommerce: {
+                    items: [{
+                        item_id: "cart_"+product.id,
+                        item_name: product.name,
+                        price: product.price,
+                        quantity: 1
+                    }]
+                }
+            });
+        }
     },
     purchase : () => {
 

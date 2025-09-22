d1ea62169b5df3d818b33b06229ada84f755622a
TeejayBala <balakumar.sivakumar@zohocorp.com>
2025-09-22 11:24:16 +0530

    website update

--- test/shopping_live/common.js
+++ test/shopping_live/common.js
@@ -20,10 +20,36 @@ window.common = {
             });
         }
     },
-    purchase : () => {
-        if (product) {
+    purchase : (items) => {
+        if (items) {
             dataLayer.push({
                 event: 'purchase',
+                ecommerce: {
+                    items: items
+                }
+            });
+        }
+    },
+    begin_checkout : (items) => {
+        var _items = [{
+            item_id: "cart_"+product.id,
+            item_name: product.name,
+            price: product.price,
+            quantity: 1
+        }];
+        if (items) {
+            dataLayer.push({
+                event: 'begin_checkout',
+                ecommerce: {
+                    items: items
+                }
+            });
+        }
+    },
+    view_item : (product) => {
+        if (product) {
+            dataLayer.push({
+                event: 'view_item',
                 ecommerce: {
                     items: [{
                         item_id: "cart_"+product.id,
@@ -34,12 +60,6 @@ window.common = {
                 }
             });
         }
-    },
-    begin_checkout : (email) => {
-
-    },
-    view_item : () => {
-
     },
     search : () => {
 
